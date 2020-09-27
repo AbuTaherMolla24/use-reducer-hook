6afd64dfc29e9498d6cb4396804cd4536fe844f9
Abu Taher Molla <57549941+AbuTaherMolla24@users.noreply.github.com>
2020-09-28 00:27:04 +0600

    mod 44 end

---
+++ src/Compinents/PatientReducer/PatientReducer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PatientReducer = () => {
+    return (
+        <div>
+            <h1>Manage hospital</h1>
+        </div>
+    );
+};
+
+export default PatientReducer;
